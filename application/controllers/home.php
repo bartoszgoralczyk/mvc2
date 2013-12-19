69ae3daa1dab8a9c308cfedc2530d594a24544e2
Bartosz <bartosz.goralczyk@terart.pl>
2013-12-19 07:39:52 +0100

    13

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,12 @@ class Home extends Controller
 {
     public function index()
     {
+	//parent::_defaultLayout = 'layouts/boot';
+	//$this->_defaultLayout = 'layouts/boot';
+	//$this->_layoutView = 'layouts/boot';
+
+	//$view = $this->getActionView();
+	//$view->setLayoutView('layouts/boot');
         // nic ciekawego...
     }
 }
